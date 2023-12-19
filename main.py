85c8349be109f6e442122b3012ea29e4ea257443
izassan <izassan.si@gmail.com>
2023-12-20 04:48:48 +0900

    update: added passing data to template

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
+json = json.load(open("services.json"))
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', services=json['services'])
 
 
 if __name__ == '__main__':
